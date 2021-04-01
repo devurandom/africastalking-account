ef6473081b1773af006de6a031a04c22bde45535
Dennis Schridde <devurandom@gmx.net>
2021-04-01 18:02:07 +0200

    Bump versions 2021/03

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   :description "Retrieve account data from Africa's Talking"
   :license {:name "AGPL-3.0-or-later"
             :url "https://www.gnu.org/licenses/agpl-3.0"}
-  :dependencies [[org.clojure/clojure "1.10.2"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/data.csv "1.0.0"]
-                 [clj-http "3.12.0"]
+                 [clj-http "3.12.1"]
                  [environ "1.2.0"]]
   :profiles {:dev {:dependencies [[philoskim/debux "0.7.9"]]}
              :uberjar {:dependencies [[philoskim/debux-stubs "0.7.9"]]}}
